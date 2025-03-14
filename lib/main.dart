f4c49f47f6b479f7b0ad2d923c6c880bdfb3b23f
Bilal Shakeel <bilalshakeel27@hotmail.com>
2025-03-14 11:26:16 +0500

    Refactored Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -37,14 +37,13 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current; // ← Add this.
 
     return Scaffold(
       body: Column(
         children: [
           Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
-
-          // ↓ Add this.
+          Text(pair.asLowerCase), // ← Change to this.
           ElevatedButton(
             onPressed: () {
               appState.getNext();
